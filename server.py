a4399dfee59c033b7e818eed9431842bd7727f27
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-06-15 13:06:49 -0300

    Implementação de cor para clientes

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 import threading
+import colorama
+from colorama import Fore, Style
+
+colorama.init()
 
 # Connection Data
 host = '127.0.0.1'
@@ -14,6 +18,8 @@ server.listen()
 clients = []
 nicknames = []
 rooms = {}
+colors = {}  # Dictionary to store colors for each nickname
+available_colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
 
 # Sending Messages To All Connected Clients
 def broadcast(message, room):
@@ -25,16 +31,18 @@ def getRoom(client):
     for room in rooms:
         if client in rooms[f'{room}']:
             return room
-                
 
 # Handling Messages From Clients
 def handle(client):
     while True:
         try:
             # Broadcasting Messages
-            message = client.recv(1024)
+            message = client.recv(1024).decode('utf-8')
             room = getRoom(client)
-            broadcast(message, room)
+            nickname = nicknames[clients.index(client)]
+            color = colors[nickname]
+            colored_message = "{}{}{}".format(color, message, Style.RESET_ALL)
+            broadcast(colored_message.encode('utf-8'), room)
         except:
             # Removing And Closing Clients
             index = clients.index(client)
@@ -42,10 +50,10 @@ def handle(client):
             room = getRoom(client)
             client.close()
             nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('utf-8'), room)
-            nicknames.remove(nickname)
+            broadcast('{}{} left!{}'.format(colors[nickname], nickname, Style.RESET_ALL).encode('utf-8'), room)
+            nicknames.remove(nickname) 
+            colors.pop(nickname)  # Remove color association
             break
-            
 
 # Receiving / Listening Function
 def receive():
@@ -60,6 +68,10 @@ def receive():
         nicknames.append(nickname)
         clients.append(client)
         
+        # Assign a color to the nickname
+        color = available_colors[len(colors) % len(available_colors)]
+        colors[nickname] = color
+
         # Request and Store Room
         client.send('ROOM'.encode('utf-8'))
         room = client.recv(1024).decode('utf-8')
@@ -70,16 +82,14 @@ def receive():
         except KeyError: 
             rooms[f'{room}']=[client]
 
-
         # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
-        broadcast("{} joined room {}!".format(nickname,room).encode('utf-8'), room)
+        broadcast("{}{} joined room {}!{}".format(color, nickname, room, Style.RESET_ALL).encode('utf-8'), room)
         client.send('Connected to server!'.encode('utf-8'))
         print("Rooms: {}".format(rooms))
 
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-        
 
 receive()
